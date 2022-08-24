d726debd3708424a45c49f037e92fd9c25722529
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-24 16:00:31 +0700

    исправление теста

--- part3/part3_lesson6_step9/test_items.py
+++ part3/part3_lesson6_step9/test_items.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 
 def test_btn_add_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
 
