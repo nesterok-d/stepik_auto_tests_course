5a006b12ff16fc41adc944d4276a27bbd86dc9d3
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-24 17:27:14 +0700

    коррекция функции Options

--- part3/part3_lesson6_step9/conftest.py
+++ part3/part3_lesson6_step9/conftest.py
@@ -2,16 +2,14 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--user_language') #, action='store', default=None, help="Choose language: es or ru"
+    parser.addoption('--user_language', action='store', default=None, help="Choose language: es or ru") #
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("user_language")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    #browser = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome(chrome_options=options, executable_path=r"chromedriver.exe")
-    driver = browser.set_driver(driver)
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
