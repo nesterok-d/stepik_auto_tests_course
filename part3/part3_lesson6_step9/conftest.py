d6868c4f1874738bb0230fbb40a5ab811eadcacd
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-24 16:11:52 +0700

    удаление лишнего кода

--- part3/part3_lesson6_step9/conftest.py
+++ part3/part3_lesson6_step9/conftest.py
@@ -3,14 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language') #, action='store', default=None, help="Choose language: es or ru"
+    parser.addoption('--user_language') #, action='store', default=None, help="Choose language: es or ru"
 
 @pytest.fixture()
 def browser(request):
-    user_language = request.config.getoption("language")
+    user_language = request.config.getoption("user_language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(options)
 
     yield browser
     print("\nquit browser..")
