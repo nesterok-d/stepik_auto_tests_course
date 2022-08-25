174fa61b2c5f767deba70b76563e6599c78e5b49
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 14:23:29 +0700

    added methods for adding to basket and finding success_massage

--- final_project/pages/product_page.py
+++ final_project/pages/product_page.py
@@ -10,14 +10,14 @@ class ProductPage(BasePage):
 
     # метод для нахождения кнопки добавления товара в корзину
     def should_be_btn_add_to_basket(self):
-        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "btn_add_to_basket is not presented"
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "btn_add_to_basket is not presented"
 
     # метод для добавления товара в корзину
     def add_product_to_basket(self):
-        btn_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        btn_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         btn_to_basket.click()
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
+
+    #метод поиска сообщения об успешном добавлении в корзину
+    def should_be_success_massage(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "сообщение о добавлении товара в корзину не получено"
+
