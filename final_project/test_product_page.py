c301b9adef85b57a6d119ef2b69f60f590c1e0e8
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 16:21:07 +0700

    added test for checking that element did not present

--- final_project/test_product_page.py
+++ final_project/test_product_page.py
@@ -31,4 +31,32 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_success_message()
     assert page.take_success_message() == page.take_title_the_book()
 
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
     time.sleep(1)
+    page.should_be_btn_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_be_btn_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_is_disappeared_success_message()
