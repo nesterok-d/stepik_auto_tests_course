d44b8e57e2ac71a6f4b44770ab4407756ed9b703
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 16:40:37 +0700

    changed main page method

--- final_project/pages/main_page.py
+++ final_project/pages/main_page.py
@@ -6,12 +6,5 @@ from .locators import MainPageLocators
 
 #создание класса "основная станица"
 class MainPage(BasePage):
-
-    # создание метода для поиска ссылка на логин
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
-    #создание метода для пеерхода к странице логина
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
