187b08d66b3bbb1233816898eb78a497857df36a
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 18:38:34 +0700

    added test user can see success message

--- final_project/test_product_page.py
+++ final_project/test_product_page.py
@@ -64,26 +64,61 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_is_disappeared_success_message()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                                            # открываем страницу
-    page.go_to_login_page()                                # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)   # открываем страницу с формами логина и регистрации
-    login_page.should_be_login_page()                      # выполняем проверку на отображение страницы
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()
-    page.is_element_present(*BasePageLocators.BASKET_LINK)
-    page.go_to_basket_page()
-    basket = BasketPage(browser, browser.current_url)
-    basket.should_be_message_in_basket()
-    basket.should_be_no_items_in_basket()
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.go_to_login_page()                                # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)   # открываем страницу с формами логина и регистрации
+        login_page.should_be_login_page()                      # выполняем проверку на отображение страницы
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
+
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        login_page.register_new_user(email, password)
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
