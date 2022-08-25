825ee5a10bd16f34ad610cf47d018feb33def715
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 11:21:21 +0700

    changed locators

--- final_project/pages/main_page.py
+++ final_project/pages/main_page.py
@@ -1,16 +1,17 @@
 #наследование свойств базовой страницы из класса BasePage
 from .base_page import BasePage
 
-from selenium.webdriver.common.by import By
+#наследование от MainPageLocators
+from .locators import MainPageLocators
 
 #создание класса "основная станица"
 class MainPage(BasePage):
 
     # создание метода для поиска ссылка на логин
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     #создание метода для пеерхода к странице логина
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
