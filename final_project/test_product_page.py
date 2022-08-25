cf589aadd906925b5650f6167e620e9280b46834
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 15:16:25 +0700

    added test for comparing titles of the books

--- final_project/test_product_page.py
+++ final_project/test_product_page.py
@@ -1,6 +1,8 @@
 import time
 
 #наследуем методы из класса MainPage
+import pytest
+
 from final_project.pages.product_page import ProductPage
 
 
@@ -8,13 +10,25 @@ from final_project.pages.product_page import ProductPage
 #создаем тестовые сцениарии
 
 #поиск ссылки на страницу логина
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
-    time.sleep(5)
+    time.sleep(1)
     page.should_be_btn_add_to_basket()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_success_massage()
-    time.sleep(5)
+    page.should_be_success_message()
+    assert page.take_success_message() in page.take_title_the_book()
+
+    time.sleep(1)
