6409b8198fafb3f746ad0477e99364b4ce4769f0
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 11:02:53 +0700

    added implicitly wait in init

--- final_project/pages/base_page.py
+++ final_project/pages/base_page.py
@@ -2,9 +2,11 @@
 
 class BasePage():
     #создение метода инициализации объекта класса (конструктор)
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
     #создение метода открывания страницы
     def open(self):
         self.browser.get(self.url)
