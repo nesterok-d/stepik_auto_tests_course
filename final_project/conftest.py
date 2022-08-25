3ce8b43910f84e6d600927cd51ffa457e7b65b30
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 10:59:08 +0700

    added conftest.py of new project

---
+++ final_project/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--user_language', action='store', default=None, help="Choose language: es or ru") #
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("user_language")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
