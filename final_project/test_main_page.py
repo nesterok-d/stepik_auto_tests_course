5b7b28e3a5af020ebe00394b050be62e7bb17c69
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 11:00:29 +0700

    added new test for finding login link

--- final_project/test_main_page.py
+++ final_project/test_main_page.py
@@ -1,7 +1,16 @@
 #наследуем методы из класса MainPage
-from .pages.main_page import MainPage
+from final_project.pages.main_page import MainPage
 
-#создаем тестовый сцениарий
+#создаем тестовые сцениарии
+
+#поиск ссылки на страницу логина
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+#переход на страницу логина
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
