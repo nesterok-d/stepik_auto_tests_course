dc9be5b37d9009acd629e478ef2f22a9bf41232c
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 11:10:18 +0700

    changed method should_be_login_link

--- final_project/pages/main_page.py
+++ final_project/pages/main_page.py
@@ -8,7 +8,7 @@ class MainPage(BasePage):
 
     # создание метода для поиска ссылка на логин
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
     #создание метода для пеерхода к странице логина
     def go_to_login_page(self):
