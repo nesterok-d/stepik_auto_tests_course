9b386de79da61c270943790f7b19fbce008e153b
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 11:41:28 +0700

    added locators for login and registration

--- final_project/pages/locators.py
+++ final_project/pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "input[id='id_login-username']")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "input[id='id_login-password']")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button.btn-lg[name='login_submit']")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "input[id='id_registration-email']")
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "input[id='id_registration-password1']")
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "input[id='id_registration-password2']")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "button.btn-lg[name='registration_submit']")
