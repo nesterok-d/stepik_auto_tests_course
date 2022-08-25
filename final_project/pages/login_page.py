d757561989ee375dab3f1b23d56b4260038fbbcb
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-25 18:37:17 +0700

    added method for registration

--- final_project/pages/login_page.py
+++ final_project/pages/login_page.py
@@ -11,13 +11,21 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверка на корректный url адрес
         assert "login" in self.url, "Login url did not contain login"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        # проверка, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        # проверка, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "registration form is not presented"
+
+    def register_new_user(self, email, password):
+        email1 = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email1.send_keys(email)
+        password1 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD1)
+        password1.send_keys(password)
+        password2 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD2)
+        password2.send_keys(password)
