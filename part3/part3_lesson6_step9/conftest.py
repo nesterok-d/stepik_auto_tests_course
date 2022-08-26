b5a445dcdb90eaf273ea7ae7c23e877357450b6b
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-26 10:15:08 +0700

    change variable name

--- part3/part3_lesson6_step9/conftest.py
+++ part3/part3_lesson6_step9/conftest.py
@@ -2,11 +2,11 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--user_language', action='store', default=None, help="Choose language: es or ru") #
+    parser.addoption('--language', action='store', default=None, help="Choose language: es or ru") #
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("user_language")
+    user_language = request.config.getoption("language")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
